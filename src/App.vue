309af4bbe60055051bc1efda27642473602dd7f2
Brayan Camargo <brayan440@hotmail.com>
2019-10-31 16:23:05 -0500

    Basic Proyect

--- src/App.vue
+++ src/App.vue
@@ -16,13 +16,10 @@ export default {
 }
 </script>
 
+<!-- Global Styles -->
 <style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  @import 'sass/main';
+  #app {
+    @include fontSize(100);
+  }
 </style>
