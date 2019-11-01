803622f5c224be565c32c03fcd88bf9e97544b64
Brayan Camargo <brayan440@hotmail.com>
2019-10-31 23:12:36 -0500

    Api conected and data print

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,63 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <article class="colorsGrid">
+      <CardColor
+        v-for="post in posts.data"
+        v-bind:key="post.id"
+        :color="post.color"
+        :id="post.id"
+        :name="post.name"
+        :pantone_value="post.pantone_value"
+        :year="post.year"
+      />
+    </article>
+    <div>
+        <input type="text" v-model="currentPage">
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import CardColor from './components/CardColor.vue'
+import axios from 'axios'
 
 export default {
   name: 'app',
+  data () {
+    return{
+      loading: false,
+      posts: null,
+      error: null,
+      currentPage: 1
+    }
+  },
+  methods: {
+    getPosts () {
+      axios.get('https://reqres.in/api/colors?page='+this.currentPage)
+        .then(response => {
+          this.posts = response.data;
+        })
+        .catch(response => {
+          console.log(response);
+        });
+    }
+  },
+  created () {
+    this.getPosts();
+  },
+  watch: {
+    currentPage () {
+      this.getPosts()
+    }
+  },
   components: {
-    HelloWorld
+    CardColor
   }
 }
 </script>
 
 <!-- Global Styles -->
 <style lang="scss">
-  @import 'sass/main';
-  #app {
-    @include fontSize(100);
-  }
+  @import './sass/main';
+  @import './sass/components/flexgrid';
 </style>
---
+++ src/components/CardColor.vue
@@ -0,0 +1,51 @@
+<template>
+    <!-- New boxColor -->
+    <div class="boxColor" :style="boxStyles">
+      <div class="colorsGrid__item_info">
+        <p>
+          {{color}}
+        </p>
+        <p>
+          {{id}}
+        </p>
+        <p>
+          {{name}}
+        </p>
+        <p>
+          {{pantone_value}}
+        </p>
+        <p>
+          {{year}}
+        </p>
+      </div>
+    </div>
+    <!-- END boxColor -->
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    color: String,
+    id: String,
+    name: String,
+    pantone_value: String,
+    year: String,
+  },
+  computed: {
+    boxStyles(){
+      return{
+        "background-color": this.color,
+      }
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+.boxColor{
+    width: calc(50% - 20px);
+    margin: 10px 10px;
+}
+</style>
